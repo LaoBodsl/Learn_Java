bafb0f1a9607218eecbca56bc4dbcd4de722004c
laobo <laobodslr@gmail.com>
2019-03-23 14:49:29 +0800

    Junit4

---
+++ JunitDemo/src/JunitDemo.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class JunitDemo {
+	@Test
+	public void demo01() {
+		System.out.println("JunitTest_Success...");
+	}
+}
--- Spring4_day01/src/laobo/SpringDemo/SpringDemo.java
+++ Spring4_day01/src/laobo/SpringDemo/SpringDemo.java
@@ -1,12 +1,13 @@
 package laobo.SpringDemo;
 
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringDemo {
-	
-	public static void main(String[] args) {
+	@Test
+	public void demo() {
 		ApplicationContext applicationContext  = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Car car = (Car) applicationContext.getBean("car");
 		System.out.println(car);
