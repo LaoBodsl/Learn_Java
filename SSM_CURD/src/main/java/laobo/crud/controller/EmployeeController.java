3fb88ee61c17557b59c625381de8151f43ed84da
laobo <laobodslr@gmail.com>
2019-06-02 14:16:48 +0800

    Json_index

--- SSM_CURD/src/main/java/laobo/crud/controller/EmployeeController.java
+++ SSM_CURD/src/main/java/laobo/crud/controller/EmployeeController.java
@@ -1,6 +1,6 @@
 package laobo.crud.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 
 import java.util.List;
 
@@ -15,6 +15,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import laobo.crud.bean.Employee;
+import laobo.crud.bean.Msg;
 import laobo.crud.service.EmployeeService;
 
 @Controller
@@ -23,7 +24,20 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeSercice;
 	
-	@RequestMapping(value="/emps")
+	@RequestMapping("/emps")
+	@ResponseBody
+	public Msg getEmpWithJson(@RequestParam(value="pn", defaultValue="1") Integer pn) {
+		//在查询之前只需压迫调用,传入页码,以及每页的大小
+		PageHelper.startPage(pn,5);
+		//startpage后面紧跟的这个查询就是一个分页查询
+		List<Employee> emps = employeeSercice.getAll();
+		//使用pageinfo包装查询后的结果,只需要将pageinnfo交给页面就行了
+		PageInfo page = new PageInfo(emps, 5);
+		return Msg.success().add("pageInfo", page);
+	}
+	
+	
+//	@RequestMapping(value="/emps")
 	public String getEmps(@RequestParam(value="pn", defaultValue="1") Integer pn, Model model) {
 		//在查询之前只需压迫调用,传入页码,以及每页的大小
 		PageHelper.startPage(pn,5);
